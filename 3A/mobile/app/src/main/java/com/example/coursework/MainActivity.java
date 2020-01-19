d21855f76081cff4b37bee42246e831cd221ac88
domgoodwin <d0m182goodwin@gmail.com>
2020-01-19 13:35:44 +0000

    Initial mobile dev work

---
+++ 3A/mobile/app/src/main/java/com/example/coursework/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.coursework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView welcome = findViewById(R.id.welcome);
+        welcome.setText("abc");
+    }
+}
