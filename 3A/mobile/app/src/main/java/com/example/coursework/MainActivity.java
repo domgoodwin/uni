7ee3d3f49848ed60eabb7b63449400666de6ff40
Dom Goodwin <d0m182goodwin@gmail.com>
2020-01-19 14:08:17 +0000

    Initial work

--- 3A/mobile/app/src/main/java/com/example/coursework/MainActivity.java
+++ 3A/mobile/app/src/main/java/com/example/coursework/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.coursework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Gallery;
 import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView welcome = findViewById(R.id.welcome);
-        welcome.setText("abc");
+
+    }
+
+    public void showGallery(View view) {
+        TextView welcome = findViewById(R.id.mainMessage);
+        welcome.setText("Going to gallery");
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
+    }
+
+    public void showHolidays(View view) {
+        TextView welcome = findViewById(R.id.mainMessage);
+        welcome.setText("Going to holidays");
+        Intent intent = new Intent(this, HolidayActivity.class);
+        startActivity(intent);
     }
 }
