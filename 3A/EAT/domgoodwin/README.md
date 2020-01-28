a1e0358c5860cce39d4aa5ef0461214f19a5152f
Dom Goodwin <d0m182goodwin@gmail.com>
2020-01-28 21:37:47 +0000

    Added readme

---
+++ 3A/EAT/domgoodwin/README.md
@@ -0,0 +1,14 @@
+# DC3160 Coursework Dom Goodwin
+
+## Questions attempted
+- Section 1
+    - a) login
+    - b) viewTimetable
+    - c) chooseLesson
+    - d) viewSelection
+    - e) logOut
+- Section 2
+    - Users restricted on number of lessons
+    - Cancel buttons on viewSelection page
+    - Disabled buttons on viewTimetale page
+    - Disabled buttons where already in selected session
--- 3A/EAT/domgoodwin/src/java/controller/Controller.java
+++ 3A/EAT/domgoodwin/src/java/controller/Controller.java
@@ -27,6 +27,7 @@ public class Controller extends HttpServlet {
    private Users users;
    private LessonTimetable availableLessons;
 
+    // Get available lessons and store with application scope
     public void init() {
          users = new Users();
          availableLessons = new LessonTimetable();
@@ -35,6 +36,7 @@ public class Controller extends HttpServlet {
         
     }
     
+    // On tomcat shutdown make sure to cleanup objects
     public void destroy() {
         availableLessons.cleanUp();
         availableLessons = null;
@@ -65,45 +67,51 @@ public class Controller extends HttpServlet {
         RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");
         HttpSession session = request.getSession(false);
         
+        // Login action (a)
         if (action.equals("/login")){
             Integer userID = users.isValid(request.getParameter("username"), request.getParameter("password"));
+            // -1 used as invalid user
             if (userID != -1) {
+                // Setup session and objects
                 System.out.println("Valid user login: "+request.getParameter("username"));
                 session = request.getSession();
                 session.setAttribute("user", request.getParameter("username"));
                 session.setAttribute("userID", userID);
                 LessonSelection sel = new LessonSelection(userID);
                 session.setAttribute("selectedLessons", sel);
-                
-                session.setAttribute("lessons", availableLessons.getLessons());
                 dispatcher = this.getServletContext().getRequestDispatcher("/do/viewTimetable");
             } else {
+                // If login failed just go back to login page
                 dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");
             }
+        // Check session exists and user set before trying any other actions
         }else if (session == null || session.getAttribute("user").equals("")){
             dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");
         } 
         else if (session.getAttribute("user") != null) {
+            // Log out user and invalidate session
             if (action.equals("/logOut")) {
                 System.out.println("Logging user out");
                 session.invalidate();
                 dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");
                 request.getSession(false);
             } else {
+                // Most actions use Lessonselection so get out here
                 LessonSelection sel = (LessonSelection) session.getAttribute("selectedLessons");
                 if (sel != null) {
                     Integer numChosen = sel.getNumChosen();
                     session.setAttribute("selectedCount", numChosen);
                 }
+                // Render base view timetable page, lessons use application scope
                 if (action.equals("/viewTimetable")) {
-                    session.setAttribute("selectedCount", sel.getNumChosen());
                     dispatcher = this.getServletContext().getRequestDispatcher("/LessonTimetableView.jspx");
                 } 
+                // Render view selection page, uses selectedLessons from session
                 else if (action.equals("/viewSelection")) {
-                    session.setAttribute("selectedCount", sel.getNumChosen());
                     System.out.println("Number selected: "+Integer.toString(sel.getNumChosen()));
                     dispatcher = this.getServletContext().getRequestDispatcher("/LessonSelectionView.jspx");
                 } 
+                // Add a lesson to users selected lesson object in session
                 else if (action.equals("/chooseLesson")) {
                     String lessonID = request.getParameter("id");
                     Lesson selectedLesson = this.availableLessons.getLesson(lessonID);
@@ -112,8 +120,10 @@ public class Controller extends HttpServlet {
                     if (! sel.getChosenLessons().containsKey(lessonID)){
                         sel.addLesson(new Lesson(selectedLesson));
                     }
+                    session.setAttribute("selectedLessons", sel);
                     dispatcher = this.getServletContext().getRequestDispatcher("/LessonSelectionView.jspx");
                 }
+                // Finalise booking by writing to DB
                 else if (action.equals("/finaliseBooking")){
                     System.out.println("Finalising booking");
                     if (sel != null) {
@@ -126,16 +136,19 @@ public class Controller extends HttpServlet {
                     session.setAttribute("selectedLessons", selNew);
                     dispatcher = this.getServletContext().getRequestDispatcher("/LessonTimetableView.jspx");
                 }
+                // Remove lesson from users selected session object
                 else if (action.equals("/cancelSelection")){
                     sel.removeLesson(request.getParameter("id"));
+                    session.setAttribute("selectedLessons", sel);
                     dispatcher = this.getServletContext().getRequestDispatcher("/LessonSelectionView.jspx");
                 }
+                // Invalidate session to log user out
                 else if (action.equals("/logOut")) {
                     dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");
                     session.invalidate();
                 }
             }
-
+        // If user isn't set and log otu request ensure user logged out anyway
         } else if (action.equals("/logOut")) {
             System.out.println("Logging user out and invalidating session");
             if (session != null){
